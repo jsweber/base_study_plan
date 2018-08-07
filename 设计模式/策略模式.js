c2f05b351b340fd5463d39342babaeb41fd8c023
duxq <dxq.duxq@lieluobo.com>
2018-08-07 16:15:50 +0800

    策略模式增加案例

--- 设计模式/策略模式.js
+++ 设计模式/策略模式.js
@@ -20,5 +20,69 @@ var calcBouns = function(level, salary){
 
 console.log(calcBouns('S', 20000))
 
+/**
+ * 动画类
+ * 
+*/
+
+const tween = {
+	linear: function(usedTime, start, dis, allTime){
+		return usedTime / allTime * dis + start
+	},
+	easeIn: function(usedTime, start, dis, allTime){
+		return (usedTime /= allTime) * dis + start
+	},
+	strongEaseIn: function(usedTime, start, dis, allTime){
+		return (usedTime /= allTime) * usedTime * usedTime * dis + start
+	},
+	strongEaseOut: function(usedTime, start, dis, allTime){
+		return ((usedTime = usedTime/allTime - 1) * usedTime * usedTime +1) * dis + start
+    }
+}
+
+
+class Animate{
+    constructor(dom){
+        this.dom = dom
+        this.startTime = 0
+        this.startPos = 0
+        this.endPos = 0
+        this.propertyName = null
+        this.easing = null
+        this.duration = null
+    }
+
+    start(propertyName, endPos, duration, easing){
+        this.startTime = +new Date()
+        this.startPos = this.dom.getBoundingClientRect()[propertyName]
+        this.propertyName = propertyName
+        this.endPos = endPos
+        this.duration = duration
+        this.easing = tween[easing]
+
+        let timer = setInterval(()=>{
+            if (!this.step()){
+                clearInterval(timer)
+            }
+        }, 20)
+    }
+
+    step(){
+        let t = +new Date()
+        let pos = 0
+
+        if (t >= this.start + this.duration){
+            this.update(this.endPos)
+            return false
+        }
+        pos = this.easing(t - this.startTime, this.startPos, this.endPos - this.startPos, this.duration)
+        this.update(pos)
+        return true
+    }
+
+    update(pos){
+        this.dom.style[this.propertyName] = pos +'px'
+    }
+}
 
 
