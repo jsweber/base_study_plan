38c44e0457586f99aaad3186a4f82257090e0b57
ecuster <951092973@qq.com>
2018-08-15 00:49:18 +0800

    增加比较排序速度的方法

---
+++ 算法/排序/StopWatch.java
@@ -0,0 +1,16 @@
+public class StopWatch{
+	private final long start;
+	
+	public StopWatch(){
+		start = System.currentTimeMillis();
+	}
+	
+	public double stop(){
+		long now = System.currentTimeMillis();
+		return (now - start) / 1000;
+    }
+    
+    public static void main(String[] args){
+
+    }
+}
