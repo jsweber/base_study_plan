67f529b244fb239c66619e99739061b6e545597e
duxq <dxq.duxq@lieluobo.com>
2018-08-20 11:37:10 +0800

    新增命令模式案例和参考书本

--- 设计模式/命令模式.js
+++ 设计模式/命令模式.js
@@ -40,5 +40,40 @@ let refreshMenuBarCommand = RefreshMenuBarCommand(MenuBar)
 setCommand(bt1, refreshMenuBarCommand)
 
 
+//Macro命令，和我们打游戏时给键盘的某些按键设置宏命令一样，就是一键执行多个任务
+let openTVCommand = {
+    execute(){
+        console.log('开电视')
+    }
+}
+let openAirConditioningCommand = {
+    execute(){
+        console.log('开空调')
+    }
+}
 
+let MacroCommand = function(){
+    return {
+        commandList: [],
+        add(command){
+            this.commandList.push(command)
+        },
+        execute(){
+            while(this.commandList.length > 0){
+                this.commandList.shift().execute()
+            }
+        }
+    }
+}
 
+//unit test
+let macro = MacroCommand()
+macro.add(openTVCommand)
+macro.add(openAirConditioningCommand)
+macro.execute()
+
+//上面没有接受者的command虽然和策略模式很像，
+//但是它们意图不同
+//策略模式的目标一致，只是解决同一个问题，不过方法不同，
+//而命令模式的问题域更广，因为它可以接受recevier，就算没有recevier的智能命令，其解决的问题也是不同的
+//此外命令模式还有撤销，排队等功能
