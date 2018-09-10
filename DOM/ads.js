27fdf60637eabbf31b03cdbd9c8c57e8f6f50240
duxq <dxq.duxq@lieluobo.com>
2018-09-10 21:38:52 +0800

    增加dom 2级文档

--- DOM/ads.js
+++ DOM/ads.js
@@ -206,6 +206,21 @@
         }
     }
 
+    var node = {
+        ELEMENT_NODE :1,
+        ATTRIBUTE_NODE : 2,
+        TEXT_NODE: 3,
+        CDATA_SECTION_NODE: 4,
+        ENTITY_REFERENCE_NODE: 5,
+        ENTITY_NODE: 6,
+        PROCESSING_INSTRUCTION_NODE: 7,
+        COMMENT_NODE: 8,
+        DOCUMENT_NODE: 9,
+        DOCUMENT_TYPE_NODE: 10,
+        DOCUMENT_FRAGMENT_NODE: 11,
+        NOTATION_NODE: 12
+    }
+
 
     win['ads']['isCompatible'] = isCompatible
     win['ads']['isNode'] = isNode
@@ -218,6 +233,7 @@
     win['ads']['prependChild'] = prependChild
     win['ads']['log'] = new myLogger()
     win['ads']['getBrowserWindowSize'] = getBrowserWindowSize
+    win['ads']['node'] = node
 
  })(window, document)
 
