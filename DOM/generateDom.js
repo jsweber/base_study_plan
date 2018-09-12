8bd7b74b3d602c8c8c467fe87072c921f7bffedc
ecuster <951092973@qq.com>
2018-09-13 02:33:46 +0800

    commit

--- DOM/generateDom.js
+++ DOM/generateDom.js
@@ -1,12 +1,20 @@
 (function(){
     function encode(str){
-
-
+        if (!str) return null
+        str = str.replace(/\\/g, '\\\\')
+        str = str.replace(/';/g, "\\'")
+        str = str.replace(/\s+^/mg, "\\n")
+        return str
     }
 
     function checkForValue(v){
-
-
+        if (!~v.indexOf('$')){
+            v = '\'' + v +'\''
+        }else{
+            v = v.substr(v.indexOf('$')+1)
+            requireVariable += 'var ' + v + ';\n'
+        }
+        return v
     }
 
     var domCode = '',
