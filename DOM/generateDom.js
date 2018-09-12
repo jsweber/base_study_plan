4d44bce40ad91b14b64ef9c5212a95b81074d658
ecuster <951092973@qq.com>
2018-09-13 01:20:37 +0800

    创建generateDom

---
+++ DOM/generateDom.js
@@ -0,0 +1,31 @@
+(function(){
+    function encode(str){
+
+
+    }
+
+    function checkForValue(v){
+
+
+    }
+
+    var domCode = '',
+    nodeNameCounters = [],
+    requireVariable = '',
+    newVariables = ''
+
+    function generate(strHTML, strRoot){
+
+    }
+
+    function processAttribute(tabCount, refParent){
+
+    }
+
+    function processNode(tabCount, refParent){
+
+    }
+
+    window['generateDom'] = generate
+
+})()
