f968b6cf1ca42975a1d8b64b781613a4734e361e
jsweber <951092973@qq.com>
2018-12-28 16:00:54 +0800

    add truncate

--- 框架学习/JavaScript框架设计/string/index.js
+++ 框架学习/JavaScript框架设计/string/index.js
@@ -1,4 +1,8 @@
-//重复字符串
+/**
+ * 重复字符串
+ * @param {*} str 
+ * @param {*} n 
+ */
 function repeat(str, n){
     return new Array(n).fill(str).join('')
 }
@@ -49,5 +53,68 @@ function repeat6(str, n){
     return n <=0 ? '' :  str.concat(repeat6(str, --n))
 }
 
+/**
+ * 获取字符串字节数
+ * 汉字的Unicode编码大于255，英文数字等一个字节就可以了，一般两个字节代表一个汉字，所意当有个汉字时，字符串长度+1
+ * 
+ */
 
+ function byteLen(str){
+     var len = str.length
+     for(var i = 0; i < str.length; i++){
+         if (str[i].charCodeAt() > 255){
+             len ++
+         }
+     }
+     return len
+ }
 
+ //mysql需要3个字节表示一个汉字，所以我们用另一种方法实现，并且支持传入用多少个字节表示汉字
+ function byteLen2(str, fix){
+     fix = fix ? fix : 2
+     let placeholder = new Array(fix + 1).join('-')
+     str = str.replace(/[^\x00-\xff]/g, placeholder)
+     return str.length
+ }
+
+ //腾讯的方法
+ function byteLen3(str, charset){
+     var total = 0,
+     i = 0,
+     c = 0
+     charset = charset ? charset.toLowerCase() : ''
+    console.log(charset)
+     if (charset === 'utf-16' || charset === 'utf16'){
+         for (; i < str.length; i++){
+             c = str.charAt(i).charCodeAt() 
+            
+             if (c <= 0x00FFFF){
+                console.log(len)
+                total+= 2
+             }else {
+                total += 4
+             }
+         }
+     }else {
+        for (; i < str.length; i++){
+            c = str.charAt(i).charCodeAt()
+            if (c <= 0x007F){// 0x00007F前面加几个0都一样
+                total++
+            }else if (c <= 0x07FF){
+                total += 2
+            }else if (c <= 0xFFFF){
+                total += 3
+            }else {
+                total += 4
+            }
+        }
+     }
+     return total
+ }
+
+//当字符串过长，时阶段添...
+function truncate(str, length, truncation){
+    truncation = truncation ? truncation : '...'
+    length = length || 30
+    return str.length > length ? str.slice(0, length - truncation.length) + truncation : String(str)
+}
