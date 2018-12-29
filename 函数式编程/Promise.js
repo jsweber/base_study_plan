875dffcc68ff6392622182a2d87f26a304acc69d
duxq <dxq.duxq@lieluobo.com>
2018-12-29 17:18:47 +0800

    update promise

--- 函数式编程/Promise.js
+++ 函数式编程/Promise.js
@@ -3,6 +3,7 @@
 
 function Promise(fn){
     this._state = 0 //状态标记，一旦确定不可更改
+    this._value = void(0) //存储返回的值
     doResolve(fn, this)
 }
 
@@ -36,7 +37,18 @@ Promise.prototype.then = function(onFullfilled, onRejected){
 
 function resolve(self, newValue){
     try {
+
+        if (newValue instanceof Promise){
+            self._state = 3
+            self._value = newValue
+            finale(self)
+            return
+        }else if(typeof then === 'function'){
+            doResolve(bind(then, newValue), self)
+            return
+        }
         self._state = 1
+        self._value = newValue
     }catch(err){
         reject(self, err)
     }
@@ -44,11 +56,39 @@ function resolve(self, newValue){
 
 function reject(self, newValue){
     self._state = 2
+    self._value = newValue
 
     if (!self._handled){
         Promise._unhandledRejectionFn(self._value)
     }
 }
 
+function handle(self, deferred){
+
+
+    setTimeout(function(){
+        var cb = self._state === 1 ? deferred.onFullfilled : deferred.onRejected
+        if (cb === null){
+            (self._state === 1 ? resolve : reject)(deferred.promise, self._value)
+            return
+        }
+        var ret
+        try{
+            ret = cb(self._value)
+        }catch(e){
+            reject(deferred.promise, e)
+            return
+        }
+        resolve(deferred.promise, ref)
+    }, 0)
+
+
+}
+
+function bind(fn, ctx){
+    return function(){
+        return fn.apply(ctx, [].slice.arguments)
+    }
+}
 
 
