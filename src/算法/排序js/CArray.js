af8daf5356a41bc6e0a7a8df157d2c3a2b6d389c
jsweber <951092973@qq.com>
2019-02-16 04:10:18 +0800

    新增插入排序

--- src/算法/排序js/CArray.js
+++ src/算法/排序js/CArray.js
@@ -58,12 +58,57 @@ class CArray{
             }
         }
     }
+    //选中排序
+    selectSort(){
+        let min = 0, arr = this.dataStore, len = this.dataStore.length
+        for (let i = 0; i < len - 1; i++){
+            min = i
+            for (let j = i + 1; j < len; j++){
+                if (arr[min] > arr[j]){
+                    min = j
+                }
+            }
+            this.swap(arr, min, i)
+        }
+    }
+
+    //插入排序
+    insertSort(){
+        let temp = 0, j = 0, arr = this.dataStore, len = this.dataStore.length
+
+        for (let i = 1; i < len; i++){
+            temp = arr[i]
+            j = i
+            while(j > 0 && (arr[j - 1] > temp)){
+                arr[j] = arr[j - 1]
+                j--
+            }
+            arr[j] = temp            
+        }
+    }
 }
 //test
-let ca = new CArray(10)
+// let num = 10000
+// let ca = new CArray(num)
+// ca.setData()
+// let startTime = +new Date()
+// ca.bubbleSort()
+// console.log(+new Date() - startTime)
+
+// ca.setData()
+// startTime = +new Date()
+// ca.selectSort()
+// console.log(+new Date() - startTime)
+
+// ca.setData()
+// startTime = +new Date()
+// ca.insertSort()
+// console.log(+new Date() - startTime)
+
+let num = 10
+let ca = new CArray(num)
 ca.setData()
-ca.toString()
-ca.bubbleSort2()
+ca.insertSort()
 ca.toString()
 
 // module.exports = CArray
