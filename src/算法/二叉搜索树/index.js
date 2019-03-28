33f43264063741b2a353142ef8a34a954bc190dc
jsweber <951092973@qq.com>
2019-03-28 08:40:05 +0800

    bst update

--- src/算法/二叉搜索树/index.js
+++ src/算法/二叉搜索树/index.js
@@ -31,12 +31,12 @@ class BST{
     putByRoot(node, key, val){
         if (node === null) return new Node(key, val, 1)
         if (node.key < key) node.right = this.putByRoot(node.right, key, val)
-        if (node.key < key) node.left = this.putByRoot(node.left, key, val)
+        if (node.key > key) node.left = this.putByRoot(node.left, key, val)
         if (node.key === key){
             node.val = val
-            node.N = this.nodeSize(node.left) + this.nodeSize(node.right) + 1
-            return node
         }
+        node.N = this.nodeSize(node.left) + this.nodeSize(node.right) + 1
+        return node
     }
 
     min(){
@@ -50,7 +50,13 @@ class BST{
     }
 
     max(){
+        if (this.root === null) return null
+        let cur = this.root
+        while(cur.right){
+            cur = cur.right
+        }
 
+        return cur
     }
 
     floor(key){
@@ -70,10 +76,26 @@ class BST{
     }
 
     ceil(key){
-
+        let x = this._ceil(this.root, key)
+        if (x === null) return null
+        return x.key
     }
 
     _ceil(node, key){
+        if (node === null) return null
+
+        if (node.key === key) return node
+        if (node.key < key) return this._ceil(node.right, key)
+        let x = this._ceil(node.left, key)
+        if (x === null) return x
+        return node
+    }
+
+    select(){
+
+    }
+
+    rank(){
         
     }
 
