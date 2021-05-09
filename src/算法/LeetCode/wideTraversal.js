e4e493c300dd9c7bc16f86ed4a19350983ea202d
ecuster <951092973@qq.com>
2021-05-09 21:59:21 +0800

    fix: wide traversal

--- src/算法/LeetCode/wideTraversal.js
+++ src/算法/LeetCode/wideTraversal.js
@@ -1,15 +1,19 @@
 // 广度优先算法
-const wideTraversal = (node) => {
-    let nodes = [], i = 0
-    while(node !== null){
-        nodes.push(node)
-        node = nodes[i++]
-        for (let i = 0; i < node.children.length; i++){
-            nodes.push(node.children[i])
+const wideTraversal = node => {
+    if (!node) return []
+    const queue = [], nodes = []
+
+    queue.push(node)
+
+    while(queue.length > 0){
+        const item = queue.shift()
+
+        nodes.push(item)
+
+        for (let i = 0; i < item.children.length; i++){
+            queue.push(item.children[i])
         }
     }
 
     return nodes
 }
-
-
